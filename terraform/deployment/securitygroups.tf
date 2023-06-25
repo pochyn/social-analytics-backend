c84e8847d2b2570744009764c0a8c750bb99bc8c
Stas Pochynok <staspochyn@gmail.com>
2023-06-25 19:28:09 -0400

    update again

--- terraform/deployment/securitygroups.tf
+++ terraform/deployment/securitygroups.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "alb" {
     protocol    = "tcp"
     from_port   = 4000
     to_port     = 4000
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
   egress {
     protocol    = "-1"
