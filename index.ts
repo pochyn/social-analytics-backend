3456ad2e3fb203e44beda7766d1270a3702de497
Stas Pochynok <staspochyn@gmail.com>
2023-06-25 18:39:42 -0400

    check

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req: Request, res: Response) => res.send("Express app works!"));
+
+app.listen(port, () => console.info(`Express listening on port ${port}!`));
