9dcc0ee7cdf8f64b51d883796cdc3db94ca3c368
Stas Pochynok <staspochyn@gmail.com>
2023-07-12 12:44:03 -0400

    check

--- terraform/deployment/securitygroups.tf
+++ terraform/deployment/securitygroups.tf
@@ -22,13 +22,13 @@ resource "aws_security_group" "alb" {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     protocol    = "-1"
